f114e15a6e078d02d3daf8a6dbe225c44c560329
Arun Dubey <arundubey87@gmail.com>
2026-09-20 17:32:57 +0100

    initial code added

---
+++ environments/dev/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend"
+    storage_account_name = "aibasedtodo"
+    container_name       = "tfstate"
+    key                  = "dev-axion-microservice/terraform.tfstate"
+    use_oidc             = true
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
