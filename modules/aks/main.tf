707786a505a83b1fa44b087dcc0aa8c444a65dad
Arun Dubey <arundubey87@gmail.com>
2026-09-20 18:31:55 +0100

    oidic issuer enabled

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -6,9 +6,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = each.value.resource_group_name
   dns_prefix          = each.value.dns_prefix
   kubernetes_version  = each.value.kubernetes_version
-  tags                = each.value.
+  tags                = each.value.tags
   oidc_issuer_enabled = true
-
   default_node_pool {
     name       = each.value.default_node_pool.name
     node_count = each.value.default_node_pool.node_count
